1199c3e4633eb217b44a8c8a4bc8a686aa370fee
unknown <nataliia.pelenia@gmail.com>
2022-10-25 05:05:16 +0900

    init node server & react frontend app

---
+++ routes/api/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const router = express.Router();
+
+// @route    GET api/auth
+// @desc     Test route
+//@access    Public
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+module.exports = router;
