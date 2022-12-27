9b9c72e3316f7b0633b2edfe1d832d98f1fd55c1
NataliiaOfBlockchain <nataliia.pelenia@gmail.com>
2022-12-27 11:13:21 +0900

    show graph on trust page

---
+++ frontend/src/views/ripple/trust/TrustList.js
@@ -0,0 +1,205 @@
+import * as React from "react";
+import { useEffect, useState } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Button, Divider, Grid, InputAdornment, OutlinedInput, Typography } from '@mui/material';
+
+// third-party
+import { io } from "socket.io-client";
+
+// project imports
+import EndorsementCard from 'ui-component/cards/EndorsementCard';
+import { gridSpacing } from 'store/constant';
+import CreateModal from "./CreateModal";
+import EndorsementCardSkeleton from 'ui-component/cards/Skeleton/EndorsementCard';
+
+import { useDispatch, useSelector } from 'store';
+import { searchEndorsements, getUsers, saveEndorsement } from 'store/slices/endorsement';
+import {openSnackbar} from "store/slices/snackbar";
+
+// assets
+import { IconSearch } from '@tabler/icons';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import {SERVER_URL} from "config";
+import useAuth from "hooks/useAuth";
+import {useParams} from "react-router-dom";
+import Empty from "ui-component/Empty";
+
+const Index = () => {
+  const theme = useTheme();
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  const [socket, setSocket] = useState(null);
+  const [page, setPage] = useState(1);
+  const [keyword, setKeyword] = useState('');
+  const [total, setTotal] = useState(0);
+  const [endorsements, setEndorsements] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [endorsement, setEndorsement] = useState({
+    text: "Trust him"
+  });
+
+  const [loading, setLoading] = React.useState(false);
+  const [errors, setErrors] = React.useState({});
+  const [openCreate, setOpenCreate] = React.useState(false);
+
+  // get all users details
+  const endorsementState = useSelector((state) => state.endorsement);
+
+  useEffect(() => {
+    setEndorsements(endorsementState.endorsements);
+    setUsers(endorsementState.users);
+    setLoading(endorsementState.loading);
+    setErrors(endorsementState.errors);
+  }, [endorsementState]);
+
+  useEffect(() => {
+    dispatch(searchEndorsements());
+    dispatch(getUsers());
+    setSocket(io(SERVER_URL));
+  }, []);
+
+  useEffect(() => {
+    if(!!userId) {
+      if(!!endorsements.find(endorsement => endorsement.user._id === userId)) {
+        setEndorsement({
+          recipient: userId,
+          text: endorsements.find(endorsement => endorsement.user._id === userId).send_text,
+          weight: endorsements.find(endorsement => endorsement.user._id === userId).send_weight
+        })
+      }
+      else {
+        setEndorsement({
+          recipient: userId,
+        })
+      }
+      setOpenCreate(true);
+    }
+  }, [userId, endorsements])
+
+  useEffect(() => {
+    if(socket) {
+      socket.on('newNotification', (notification) => {
+        if(notification.recipientId === user._id && notification.notificationType === 'TRUST') dispatch(searchEndorsements(keyword, page));
+      })
+    }
+  }, [socket]);
+
+  const handleSearchType = (e) => {
+    if(e.keyCode === 13) {
+      setKeyword(e.target.value);
+      dispatch(searchEndorsements(e.target.value, page));
+    }
+  }
+
+  const handleNewClick = () => {
+    setEndorsement({})
+    setOpenCreate(true);
+  }
+
+  const handleSaveClick = () => {
+    dispatch(saveEndorsement(endorsement, successAction));
+  }
+
+  const successAction = () => {
+    dispatch(
+      openSnackbar({
+        open: true,
+        message: "You have created trust limit successfully",
+        variant: 'alert',
+        alert: {
+          color: 'success'
+        },
+        close: false
+      })
+    )
+    dispatch(searchEndorsements(keyword, page));
+    setOpenCreate(false);
+  }
+
+  return (
+    <>
+      <Grid container spacing={gridSpacing}>
+        <Grid
+          className="block"
+          item
+          xs
+          zeroMinWidth
+          sx={{ display: 'flex' }}
+        >
+          <Grid container alignItems="center" spacing={gridSpacing}>
+            <Grid item xs zeroMinWidth>
+              <OutlinedInput
+                id="input-search-card-style1"
+                placeholder="Search Contact"
+                fullWidth
+                startAdornment={
+                  <InputAdornment position="start">
+                    <IconSearch stroke={1.5} size="16px" />
+                  </InputAdornment>
+                }
+                onKeyUp={handleSearchType}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" size="large" startIcon={<AddCircleOutlineOutlinedIcon />} onClick={handleNewClick}>
+                New
+              </Button>
+            </Grid>
+            {console.log("123456789012".split(''))}
+            {
+              loading ? (
+                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
+                  {
+                    "123456789012".split('').map((i, key) => (
+                      <Grid item xs={12} md={6} lg={4} xl={3} key={key}>
+                        <EndorsementCardSkeleton />
+                      </Grid>
+                    ))
+                  }
+                </Grid>
+              ) : endorsements.length > 0 ? (
+                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
+                  {
+                    endorsements.map((item, i) => (
+                      <Grid item xs={12} md={6} lg={4} xl={3} key={i}>
+                        <EndorsementCard
+                          endorsement={item}
+                          onActive={() => {
+                            setEndorsement({
+                              recipient: item.user._id,
+                              text: item.send_text,
+                              weight: item.send_weight
+                            })
+                            setOpenCreate(true);
+                          }}
+                        />
+                      </Grid>
+                    ))}
+                </Grid>
+              ) : (
+                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
+                  <Empty />
+                </Grid>
+              )
+            }
+          </Grid>
+        </Grid>
+      </Grid>
+      <CreateModal
+        open={openCreate}
+        onClose={() => setOpenCreate(false)}
+        onSave={handleSaveClick}
+        users={users}
+        endorsement={endorsement}
+        setEndorsement={setEndorsement}
+        errors={errors}
+      />
+    </>
+  );
+};
+
+export default Index;
--- frontend/src/views/ripple/trust/index.js
+++ frontend/src/views/ripple/trust/index.js
@@ -1,204 +1,106 @@
-import * as React from "react";
-import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { Button, Divider, Grid, InputAdornment, OutlinedInput, Typography } from '@mui/material';
+import { Box, Tab, Tabs, Typography, styled } from '@mui/material';
 
-// third-party
-import { io } from "socket.io-client";
-
-// project imports
-import EndorsementCard from 'ui-component/cards/EndorsementCard';
-import MainCard from 'ui-component/cards/MainCard';
-import { gridSpacing } from 'store/constant';
-import CreateModal from "./CreateModal";
-import EndorsementCardSkeleton from 'ui-component/cards/Skeleton/EndorsementCard';
-
-import { useDispatch, useSelector } from 'store';
-import { searchEndorsements, getUsers, saveEndorsement } from 'store/slices/endorsement';
-import {openSnackbar} from "store/slices/snackbar";
+import TrustList from "./TrustList";
+import Graph from "../graph/index";
 
 // assets
-import { IconSearch } from '@tabler/icons';
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-import {SERVER_URL} from "config";
-import useAuth from "hooks/useAuth";
-import {useParams} from "react-router-dom";
-import Empty from "ui-component/Empty";
+import MainCard from "ui-component/cards/MainCard";
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import GraphTwoToneIcon from '@mui/icons-material/AutoGraphRounded';
+
+// tab content
+function TabPanel({ children, value, index, ...other }) {
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+      {value === index && (
+        <Box
+          sx={{
+            py: 3
+          }}
+        >
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+// icon tab style
+const AntTabs = styled(Tabs)(({ theme }) => ({
+  background: theme.palette.mode === 'dark' ? theme.palette.dark[800] : theme.palette.primary.light,
+  width: 140,
+  borderRadius: '12px',
+  '& .MuiTabs-indicator': {
+    backgroundColor: theme.palette.secondary.main
+  }
+}));
+
+// style constant
+const AntTab = styled((props) => <Tab disableRipple {...props} />)(({ theme }) => ({
+  textTransform: 'none',
+  minWidth: 0,
+  [theme.breakpoints.up('sm')]: {
+    minWidth: 0
+  },
+  fontWeight: theme.typography.fontWeightRegular,
+  marginRight: theme.spacing(1),
+  marginLeft: theme.spacing(1),
+  color: theme.palette.secondary.main,
+  fontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Roboto',
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"'
+  ].join(','),
+  '&:hover': {
+    color: theme.palette.secondary.main,
+    opacity: 1
+  },
+  '&.Mui-selected': {
+    color: theme.palette.secondary.main,
+    fontWeight: theme.typography.fontWeightMedium
+  },
+  '&.Mui-focusVisible': {
+    backgroundColor: theme.palette.secondary.main
+  }
+}));
 
 const Index = () => {
   const theme = useTheme();
-  const { userId } = useParams();
-  const dispatch = useDispatch();
-  const { user } = useAuth();
-
-  const [socket, setSocket] = useState(null);
-  const [page, setPage] = useState(1);
-  const [keyword, setKeyword] = useState('');
-  const [total, setTotal] = useState(0);
-  const [endorsements, setEndorsements] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [endorsement, setEndorsement] = useState({
-    text: "Trust him"
-  });
-
-  const [loading, setLoading] = React.useState(false);
-  const [errors, setErrors] = React.useState({});
-  const [openCreate, setOpenCreate] = React.useState(false);
-
-  // get all users details
-  const endorsementState = useSelector((state) => state.endorsement);
-
-  useEffect(() => {
-    setEndorsements(endorsementState.endorsements);
-    setUsers(endorsementState.users);
-    setLoading(endorsementState.loading);
-    setErrors(endorsementState.errors);
-  }, [endorsementState]);
-
-  useEffect(() => {
-    dispatch(searchEndorsements());
-    dispatch(getUsers());
-    setSocket(io(SERVER_URL));
-  }, []);
-
-  useEffect(() => {
-    if(!!userId) {
-      if(!!endorsements.find(endorsement => endorsement.user._id === userId)) {
-        setEndorsement({
-          recipient: userId,
-          text: endorsements.find(endorsement => endorsement.user._id === userId).send_text,
-          weight: endorsements.find(endorsement => endorsement.user._id === userId).send_weight
-        })
-      }
-      else {
-        setEndorsement({
-          recipient: userId,
-        })
-      }
-      setOpenCreate(true);
-    }
-  }, [userId, endorsements])
-
-  useEffect(() => {
-    if(socket) {
-      socket.on('newNotification', (notification) => {
-        if(notification.recipientId === user._id && notification.notificationType === 'TRUST') dispatch(searchEndorsements(keyword, page));
-      })
-    }
-  }, [socket]);
-
-  const handleSearchType = (e) => {
-    if(e.keyCode === 13) {
-      setKeyword(e.target.value);
-      dispatch(searchEndorsements(e.target.value, page));
-    }
-  }
-
-  const handleNewClick = () => {
-    setEndorsement({})
-    setOpenCreate(true);
-  }
-
-  const handleSaveClick = () => {
-    dispatch(saveEndorsement(endorsement, successAction));
-  }
-
-  const successAction = () => {
-    dispatch(
-      openSnackbar({
-        open: true,
-        message: "You have created trust limit successfully",
-        variant: 'alert',
-        alert: {
-          color: 'success'
-        },
-        close: false
-      })
-    )
-    dispatch(searchEndorsements(keyword, page));
-    setOpenCreate(false);
-  }
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <MainCard title="Trust">
-      <Grid container spacing={gridSpacing}>
-        <Grid
-          className="block"
-          item
-          xs
-          zeroMinWidth
-          sx={{ display: 'flex' }}
-        >
-          <Grid container alignItems="center" spacing={gridSpacing}>
-            <Grid item xs zeroMinWidth>
-              <OutlinedInput
-                id="input-search-card-style1"
-                placeholder="Search Contact"
-                fullWidth
-                startAdornment={
-                  <InputAdornment position="start">
-                    <IconSearch stroke={1.5} size="16px" />
-                  </InputAdornment>
-                }
-                onKeyUp={handleSearchType}
-              />
-            </Grid>
-            <Grid item>
-              <Button variant="contained" size="large" startIcon={<AddCircleOutlineOutlinedIcon />} onClick={handleNewClick}>
-                New
-              </Button>
-            </Grid>
-            {console.log("123456789012".split(''))}
-            {
-              loading ? (
-                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
-                  {
-                    "123456789012".split('').map((i, key) => (
-                      <Grid item xs={12} md={6} lg={4} xl={3} key={key}>
-                        <EndorsementCardSkeleton />
-                      </Grid>
-                    ))
-                  }
-                </Grid>
-              ) : endorsements.length > 0 ? (
-                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
-                  {
-                    endorsements.map((item, i) => (
-                      <Grid item xs={12} md={6} lg={4} xl={3} key={i}>
-                        <EndorsementCard
-                          endorsement={item}
-                          onActive={() => {
-                            setEndorsement({
-                              recipient: item.user._id,
-                              text: item.send_text,
-                              weight: item.send_weight
-                            })
-                            setOpenCreate(true);
-                          }}
-                        />
-                      </Grid>
-                    ))}
-                </Grid>
-              ) : (
-                <Grid container direction="row" spacing={gridSpacing} sx={{ padding: 3 }}>
-                  <Empty />
-                </Grid>
-              )
-            }
-          </Grid>
-        </Grid>
-      </Grid>
-      <CreateModal
-        open={openCreate}
-        onClose={() => setOpenCreate(false)}
-        onSave={handleSaveClick}
-        users={users}
-        endorsement={endorsement}
-        setEndorsement={setEndorsement}
-        errors={errors}
-      />
+      <AntTabs theme={theme} value={value} onChange={handleChange} aria-label="ant example">
+        <AntTab icon={<MenuTwoToneIcon sx={{ fontSize: '1.3rem' }} />} />
+        <AntTab icon={<GraphTwoToneIcon sx={{ fontSize: '1.3rem' }} />} />
+      </AntTabs>
+      <TabPanel value={value} index={0}>
+        <TrustList />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Graph />
+      </TabPanel>
     </MainCard>
   );
 };
