aa5d38220626c54727f3c655c1f6075feafab973
NataliiaOfBlockchain <nataliia.pelenia@gmail.com>
2023-01-22 13:15:13 +0900

    provide admin permission to dlevy

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,11 @@
 const functions = require("firebase-functions");
 
+const admin = require("firebase-admin");
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
