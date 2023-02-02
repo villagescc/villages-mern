dd84fc2213626971b442cee2c90561721437d2c1
NataliiaOfBlockchain <nataliia.pelenia@gmail.com>
2023-02-02 16:59:58 +0900

    updated emtpy card component

--- frontend/src/ui-component/Empty.js
+++ frontend/src/ui-component/Empty.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import InboxIcon from '@mui/icons-material/Inbox';
+import EmtpyBox from 'assets/images/icons/empty-box.png';
 
 const Empty = () => {
-  return (
-    <Box
-      style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: 300, border: '1px dashed grey', borderRadius: 5 }}
-    >
-        <InboxIcon sx={{ fontSize: 100 }} color={'primary'} />
-        <Typography variant={'h3'}> No Data </Typography>
-    </Box>
-  );
+    return (
+        <Box
+            style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: 300
+            }}
+        >
+            <img src={EmtpyBox} style={{ width: 200, height: 200 }} />
+            <Typography variant={'h1'} align="center">
+                Sorry, No Data To Show
+            </Typography>
+            <Typography variant={'body'} align="center">
+                There is not data what you need
+            </Typography>{' '}
+        </Box>
+    );
 };
 
 export default Empty;
