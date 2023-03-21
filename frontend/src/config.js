7647562dc0c390a3bf1b4bc8fe0cf2deec3a183b
Mike <nataliia.pelenia@gmail.com>
2023-03-21 15:12:47 +0000

    config update for frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,42 +1,42 @@
-import LAYOUT_CONST from 'constant';
+import LAYOUT_CONST from "constant";
 
-export const SERVER_URL = 'https://beta.villages.io';
+export const SERVER_URL = "https://villages.io";
 // export const SERVER_URL = 'http://localhost:5000';
 
 export const JWT_API = {
-    secret: 'SECRET-KEY',
-    timeout: '1 days'
+  secret: "SECRET-KEY",
+  timeout: "1 days",
 };
 
 export const AUTH0_API = {
-    client_id: '7T4IlWis4DKHSbG8JAye4Ipk0rvXkH9V',
-    domain: 'dev-w0-vxep3.us.auth0.com'
+  client_id: "7T4IlWis4DKHSbG8JAye4Ipk0rvXkH9V",
+  domain: "dev-w0-vxep3.us.auth0.com",
 };
 
 export const AWS_API = {
-    poolId: 'us-east-1_AOfOTXLvD',
-    appClientId: '3eau2osduslvb7vks3vsh9t7b0'
+  poolId: "us-east-1_AOfOTXLvD",
+  appClientId: "3eau2osduslvb7vks3vsh9t7b0",
 };
 
 // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
 // like '/berry-material-react/react/default'
-export const BASE_PATH = '';
+export const BASE_PATH = "";
 
-export const DASHBOARD_PATH = '/home';
+export const DASHBOARD_PATH = "/home";
 
 export const HORIZONTAL_MAX_ITEM = 6;
 
 const config = {
-    layout: LAYOUT_CONST.VERTICAL_LAYOUT, // vertical, horizontal
-    drawerType: LAYOUT_CONST.DEFAULT_DRAWER, // default, mini-drawer
-    fontFamily: `'Roboto', sans-serif`,
-    borderRadius: 8,
-    outlinedFilled: true,
-    navType: 'light', // light, dark
-    presetColor: 'default', // default, theme1, theme2, theme3, theme4, theme5, theme6
-    locale: 'en', // 'en' - English, 'fr' - French, 'ro' - Romanian, 'zh' - Chinese
-    rtlLayout: false,
-    container: false
+  layout: LAYOUT_CONST.VERTICAL_LAYOUT, // vertical, horizontal
+  drawerType: LAYOUT_CONST.DEFAULT_DRAWER, // default, mini-drawer
+  fontFamily: `'Roboto', sans-serif`,
+  borderRadius: 8,
+  outlinedFilled: true,
+  navType: "light", // light, dark
+  presetColor: "default", // default, theme1, theme2, theme3, theme4, theme5, theme6
+  locale: "en", // 'en' - English, 'fr' - French, 'ro' - Romanian, 'zh' - Chinese
+  rtlLayout: false,
+  container: false,
 };
 
 export default config;
