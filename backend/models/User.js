83a5812a6198ab00920a17a45b03b166a3f4f565
NataliiaOfBlockchain <nataliia.pelenia@gmail.com>
2023-03-30 10:51:27 +0900

    Remove firstname and lastname request

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,11 +21,11 @@ const UserSchema = new mongoose.Schema(
     },
     firstName: {
       type: String,
-      required: true,
+      required: false,
     },
     lastName: {
       type: String,
-      required: true,
+      required: false,
     },
     email: {
       type: String,
