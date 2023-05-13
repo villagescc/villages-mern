ae19a01de8087fe14f857f98fca5b0ea2af1d040
Nishan Kakadiya <nishankakadiya.vision@gmail.com>
2023-05-14 01:28:31 +0530

    deployment setup

---
+++ deploye.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the project directory
+cd /home/mike/villages-mern-git/villages-mern
+
+# Fetch the latest changes from the remote repository
+git fetch origin
+
+# Reset the working directory to the latest commit on the release branch
+git reset --hard origin/release-branch
+
+cd frontend
+
+npm i
+
+npm run build
